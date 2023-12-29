0a57e2caaaa9670f1d09c848d817628ee5cd12f3
MoeAlm <almazwzy8@gmail.com>
2023-12-29 20:16:18 +0200

    Added API Docs

--- app/app/settings.py
+++ app/app/settings.py
@@ -35,9 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'rest_framework',
     'drf-spectacular',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -125,5 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.USER'
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASSES': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
